780f12756392c3b99acc55bf9ad37d8e0e621ea4
canlong.shen <562172151@qq.com>
2022-07-21 23:35:26 +0800

    feat: 新增警报屏幕闪烁

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  * @Author: shencanlong
  * @Date: 2022-06-26 23:12:11
  * @LastEditors: git config user.name && git config user.email
- * @LastEditTime: 2022-06-27 00:22:15
+ * @LastEditTime: 2022-07-21 23:31:50
  * @FilePath: \learn-gaode-map\src\App.vue
  * @Description: 
  * 
@@ -12,7 +12,8 @@
   <div id="app">
     <!-- <map-gao-de /> -->
     <!-- <MapRipple /> -->
-    <MapSelect />
+    <!-- <MapSelect /> -->
+    <Flicker />
   </div>
 </template>
 
@@ -20,13 +21,15 @@
 <script>
 // import  MapGaoDe from '@/components/map/MapGaoDe.vue'
 // import MapRipple from "@/components/map/MapRipple.vue";
-import MapSelect from "@/components/map/MapSelect.vue";
+// import MapSelect from "@/components/map/MapSelect.vue";
+import Flicker from "@/components/map/alarme/flicker.vue";
 export default {
   name: "App",
   components: {
     // MapGaoDe
     // MapRipple,
-    MapSelect,
+    // MapSelect,
+    Flicker,
   },
 };
 </script>
---
+++ src/components/map/alarme/flicker.vue
@@ -0,0 +1,53 @@
+<!--
+ * @Author: shencanlong
+ * @Date: 2022-07-21 22:48:14
+ * @LastEditors: git config user.name && git config user.email
+ * @LastEditTime: 2022-07-21 23:31:27
+ * @FilePath: \learn-gaode-map\src\components\map\alarme\MapFlicker.vue
+ * @Description: 
+ * 
+-->
+<template>
+  <div class="map-flicker">
+    <div class="flicker"></div>
+    <audio autoplay loop>
+      <source src="./alarme.mp3" type="audio/mpeg" />
+    </audio>
+  </div>
+</template>
+<script>
+export default {
+  name: "MapFlicker",
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="scss" scoped>
+/* 自定义样式
+---------------------------------------------------------------- */
+.flicker {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  animation-duration: 1s;
+  animation-name: flicker_animation;
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
+
+  z-index: 999;
+}
+
+@keyframes flicker_animation {
+  from {
+    box-shadow: inset 0 0 4em 0.5em red;
+  }
+
+  to {
+    box-shadow: inset 0 0 4em 0.5em rgba(0, 0, 0, 0);
+  }
+}
+</style>
